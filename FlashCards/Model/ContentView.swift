0716e35218d85157109f1b8546c74d9f06dfb013
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-10 11:18:57 -0500

    Got the initial project folder structure nicely set up with Model and Views groups.

---
+++ FlashCards/Model/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  FlashCards
+//
+//  Created by Madison Dellamea on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("What is the power house of the cell?")
+            .padding()
+        
+//        Button(action: {
+//            // NOTE: Output will not be shown unless this app is run in the "full" simulator
+//            print("Button was pressed")
+//        }, label: {
+//            Text("Press me")
+//        })
+//        .buttonStyle(.bordered)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
